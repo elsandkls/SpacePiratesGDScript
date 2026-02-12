6265001451f4521adc3fa7b2cb3669b918952102
ElsAndKls <elsandkls@gmail.com>
2026-02-11 19:55:32 -0500

    started building the interface for the settings page with place holder art

--- Menus/Scripts/settings_menu.gd
+++ Menus/Scripts/settings_menu.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+
+func _on_check_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	pass # Replace with function body.
